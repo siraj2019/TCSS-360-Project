9676966007f3f87021f8d8d13f7fa8f85230dae8
jgeray2019 <56178641+jgeray2019@users.noreply.github.com>
2019-11-03 20:53:13 -0800

    Update DatabaseAbout.java

--- src/main/java/database/DatabaseAbout.java
+++ src/main/java/database/DatabaseAbout.java
@@ -7,13 +7,38 @@ public class DatabaseAbout implements DatabaseObjects {
 
     @Override
     public List<String> get() {
-        List<String> returnList = new ArrayList<>();
-
-        returnList.add("Leif");
-        returnList.add("Jesse");
-        returnList.add("Siraj");
-        returnList.add("Abdul");
-
+        List <String> returnList = new ArrayList<>();
+        String filename = "testfile.csv";
+        BufferedReader br = null;
+        String line = "";
+        String token = ",";
+        
+        try {
+            
+            br = new BufferedReader(new FileReader(filename));
+            while ((line = br.readLine()) != null) {
+                
+                String[] data = line.split(token);
+                
+                //System.out.println(data[0]);
+                
+                returnList.add(data[0]);
+            }
+        
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        
         return returnList;
     }
 
